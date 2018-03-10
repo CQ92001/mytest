126b8b903267d7be6f70dee11c7f58cc45e27238
Faker <33572075+CQ92001@users.noreply.github.com>
2018-03-10 10:14:03 +0800

    修改注释
    更改了banner的注释

--- springboot/src/main/java/cn/itcast/springboot/Application.java
+++ springboot/src/main/java/cn/itcast/springboot/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(Application.class);
-		springApplication.setBannerMode(Banner.Mode.OFF); // 关闭banner11
+		springApplication.setBannerMode(Banner.Mode.OFF); // 关闭banner
 		springApplication.run(args);
 	}
 }
